05633fb5453d6f0656f7111acf1acd3ff92a5a7c
Álvaro Cafe <alvaro.campos.ferreira@gmail.com>
2021-05-25 21:10:27 -0300

    Adicionando arquivos principais de rotas e templates.

--- routes.py
+++ routes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 PF005
 Objetivos:
@@ -5,15 +7,32 @@ Objetivos:
 
 """
 
-
-from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField # Os validadores são usados para garantir que as entradas sejam o que a gente espera.
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from flask import render_template, Blueprint, redirect, url_for, flash, request
+from flask import Flask, render_template, abort
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'minhasenha'
 
+class Form(FlaskForm):
+    usuario = StringField('Usuario', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
 
-@app.route('/')
-def hello_world():
-        return 'Hello, World!'
+@app.route("/", methods=['GET','POST'])
+def index():
+    form = Form()
+    if form.validate_on_submit():
+        print(f'Usuario: {form.usuario.data} \nSenha: {form.senha.data}')
+        return render_template('site.html')
+    else:
+        print('não validou')
 
+    return render_template('index.html',form=form)
+    # return "Hello"
 
-if __name__ == '__main__':
-    app.run()
+app.run(host='localhost', port=5002, debug=True)
+
+# if __name__ == '__main__':
+#     app.run()
